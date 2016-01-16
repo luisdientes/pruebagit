14ca46083c4a4abf65df9b66c8b29c7a0648f7ed
luisdientes <luis.palacios@crinbusolutions.com>
2016-01-16 13:33:04 +0100

    pruea

--- pruebagit/src/pruebagit/holamundo.java
+++ pruebagit/src/pruebagit/holamundo.java
@@ -4,7 +4,7 @@ public class holamundo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hola mundo");
+		System.out.println("Hola mundo333333333333");
 
 	}
 
